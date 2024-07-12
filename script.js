b411be97ef5c5ce438b81983b720685c95799a2a
AnandMathapati <0606dnana@gmail.com>
2024-07-12 12:52:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,74 @@
+let buttons = document.querySelectorAll(".btn");
+let a = ["images/paper.png", "images/stone.png", "images/cessior.png"]
+let p = "images/paper.png"
+let st = "images/stone.png"
+let s = "images/cessior.png"
+let count =0
+let no = 2
+let cursor = document.querySelector(".cursor");
+
+document.addEventListener("mousemove",(e)=>{
+    let x = e.clientX
+    let y = e.clientY
+    // console.log(x);
+    // console.log(y);
+    cursor.style.left = `${x}px`
+    cursor.style.top = `${y}px`
+})
+
+buttons.forEach((i) =>{
+    
+        i.addEventListener('click',()=>{
+             count++;
+             console.log(count);
+let select = a[Math.floor(Math.random()*a.length)]; 
+computer.src = select
+            
+player.style.animation = `shift 2s infinite  `
+computer.style.animation = `shift1 2s infinite `
+// player.style.animationDelay = no+"s";
+// computer.style.animationDelay = no+"s";
+
+            
+            if(i == document.getElementById("paper")){
+               player.src = p;
+             
+            }else if(i == document.getElementById("stone")){
+                player.src = st;
+              
+             }else {
+                player.src = s;
+              
+             }
+
+            
+             if((player.src==computer.src )){
+                r_text.innerHTML = "tied!"
+            }
+            else if((i==stone && select==p)||(i==scissor && select==st)||(i==paper && select==s) ){
+                    
+                r_text.innerHTML = "you loose!"
+                c_count.value++
+            }
+            
+            else{
+                r_text.innerHTML = "you win!"
+                p_count.value++;
+             }
+             
+           
+          
+             
+        })
+})
+setInterval(()=>{
+    no++
+    
+},1000)
+document.getElementById("reset").addEventListener("click",()=>{
+    location.reload()
+    
+    
+})
+
+
